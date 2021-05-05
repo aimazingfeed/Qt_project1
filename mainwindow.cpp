f054d6118f16ce5a9c7e4f08b5c6a4e1910e5cfa
Aimazing <46224499+aimazingfeed@users.noreply.github.com>
2021-05-06 01:47:34 +0300

    Add files via upload
    commit

---
+++ mainwindow.cpp
@@ -0,0 +1,57 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QDebug>
+#include <QDir>
+#include <QCoreApplication>
+#include <QtSql/QSqlError>
+#include <QtSql/QSqlRecord>
+#include <QDate>
+using namespace std;
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("L:/Projects/Qt_test/Qt_db.db");
+    db.setUserName("root");
+    if (!db.open()) {
+        qDebug() << db.lastError().text();
+        return;
+    } else {
+        qDebug() << "Success";
+    }
+
+}
+string MainWindow::getDataFromFile() {
+
+    QFile file(path);
+    if ((file.exists())&&(file.open(QIODevice::ReadOnly))) {
+        QString str="";
+        while(!file.atEnd()) {
+            str=str+file.readLine();
+        }
+
+        file.close();
+
+        return str.toStdString();
+    } else  {
+        qDebug() << "Error";
+        con = false;
+    }
+}
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+
+void MainWindow::on_showButton_clicked() {
+    QSqlQueryModel* query = new QSqlQueryModel;
+//    while (con) {
+    QString str = QString::fromStdString(getDataFromFile());
+    query->setQuery(str);
+    QTableView *tv = new QTableView(this);
+    tv->setModel(query);
+    ui->tableView->setModel(query);
+    }
+//}
---
+++ mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+#include <QtSql/QSqlDatabase>
+#include <QWidget>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlTableModel>
+#include <QDateTime>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    std::string getDataFromFile();
+    QSqlDatabase db;
+    QSqlTableModel* model;
+    QSqlQueryModel* query;
+
+private slots:
+    void on_showButton_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+    bool con=true;
+    QDate cd = QDate::currentDate();
+//    QString path="L:/Projects/Qt_test/" + cd.toString("dd.mm.yyyy") +".sql";
+    QString path = "L:/Projects/Qt_test/06.05.2021.sql";
+};
+#endif // MAINWINDOW_H
