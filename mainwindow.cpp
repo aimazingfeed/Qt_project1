fa606b2b2b9add582cd422b32d40e3244da5b80c
Aimazing <46224499+aimazingfeed@users.noreply.github.com>
2021-05-07 14:44:59 +0300

    upd

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,15 +6,26 @@
 #include <QCoreApplication>
 #include <QtSql/QSqlError>
 #include <QtSql/QSqlRecord>
-#include <QDate>
+#include <QDateTime>
+#include <QFileInfoList>
+#include <QtAlgorithms>
+
+
 using namespace std;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    QSqlDatabase db;
+
+//    QSqlTableModel* model;
+//    QSqlQueryModel* query;
+
     db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("L:/Projects/Qt_test/Qt_db.db");
     db.setUserName("root");
+
+
     if (!db.open()) {
         qDebug() << db.lastError().text();
         return;
@@ -22,24 +33,108 @@ MainWindow::MainWindow(QWidget *parent)
         qDebug() << "Success";
     }
 
+    dir.setPath("L:/Projects/Project_Qt1");
+    dir.setNameFilters(filters);
+    QFileInfoList list = dir.entryInfoList(QDir::Files,QDir::Time);
+    qDebug() << "list size " << list.size();
+
+    pathsFiles = sort(list);
+    for (auto i:pathsFiles)
+    {
+        qDebug() << QString::fromStdString(i);
+    }
+
+
 }
+
+std::vector<std::string> MainWindow::sort(const QFileInfoList& list) {
+    vector<std::string> buf;
+    for (size_t i = 0;i<list.size(); ++i) {
+        buf.push_back((list[i].absoluteFilePath()).toStdString());
+    }
+    for (size_t i = 0;i<buf.size(); ++i) {
+        for (size_t i = 1;i<buf.size(); ++i) {
+            if (isSecondEarlier(QString::fromStdString(buf[i-1]),QString::fromStdString(buf[i]))) {
+                swap(buf[i-1],buf[i]);
+            }
+        }
+    }
+    return buf;
+
+}
+
+
+
+
+bool MainWindow::isSecondEarlier(QString first, QString second) {
+    string strFirst, strSecond;
+    strFirst = first.toStdString();
+    strFirst.erase(strFirst.end()-4,strFirst.end());
+    strFirst.erase(0,strFirst.find_last_of('/')+1);
+
+    strSecond = second.toStdString();
+    strSecond.erase(strSecond.end()-4,strSecond.end());
+    strSecond.erase(0,strSecond.find_last_of('/')+1);
+
+
+
+
+
+    vector<string> first_tokens = split(strFirst,'.');
+    vector<string> second_tokens= split(strSecond,'.');
+
+    for (size_t i = first_tokens.size()-1; i >0 ;i--) {
+        if (stoi(first_tokens[i]) > stoi(second_tokens[i])) {
+            return true;
+        } else {
+            continue;
+        }
+    }
+    return false;
+}
+std::vector<std::string> MainWindow::split(const std::string& s, char seperator) {
+
+   std::vector<std::string> output;
+
+    std::string::size_type prev_pos = 0, pos = 0;
+
+    while((pos = s.find(seperator, pos)) != std::string::npos) {
+        std::string substring( s.substr(prev_pos, pos-prev_pos) );
+
+        output.push_back(substring);
+
+        prev_pos = ++pos;
+    }
+
+    output.push_back(s.substr(prev_pos, pos-prev_pos)); // Last word
+
+    return output;
+}
+
 string MainWindow::getDataFromFile() {
 
-    QFile file(path);
+QString str="";
+for (auto i:pathsFiles) {
+    QFile file(QString::fromStdString(i));
     if ((file.exists())&&(file.open(QIODevice::ReadOnly))) {
-        QString str="";
+
         while(!file.atEnd()) {
             str=str+file.readLine();
         }
-
-        file.close();
-
-        return str.toStdString();
+    file.close();
     } else {
         qDebug() << path;
-        con = false;
     }
 }
+return str.toStdString();
+}
+
+bool MainWindow::dtcomp(QDateTime* left, QDateTime *right) {
+  return *left < *right;
+}
+
+
+
 MainWindow::~MainWindow() {
     delete ui;
 }
@@ -47,11 +142,13 @@ MainWindow::~MainWindow() {
 
 void MainWindow::on_showButton_clicked() {
     QSqlQueryModel* query = new QSqlQueryModel;
-//    while (con) {
     QString str = QString::fromStdString(getDataFromFile());
     query->setQuery(str);
     QTableView *tv = new QTableView(this);
     tv->setModel(query);
+    ui->tableView->resizeColumnsToContents();
+    ui->tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tableView->horizontalHeader()->setStretchLastSection(true);
     ui->tableView->setModel(query);
-    }
-//}
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -6,7 +6,7 @@
 #include <QtSql/QSqlQuery>
 #include <QtSql/QSqlTableModel>
 #include <QDateTime>
-
+#include <QDir>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -20,12 +20,14 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
     std::string getDataFromFile();
-    QSqlDatabase db;
-    QSqlTableModel* model;
-    QSqlQueryModel* query;
+    bool dtcomp(QDateTime* left, QDateTime *right);
+    bool isSecondEarlier(QString first, QString second);
+    std::vector<std::string> split(const std::string&, char);
+    std::vector<std::string> sort(const QFileInfoList&);
+
 
 private slots:
-    void on_showButton_clicked();
+    void on_showButton_clicked( );
 
 
 private:
@@ -33,6 +35,9 @@ private:
     bool con=true;
     QDate cd = QDate::currentDate();
     QString path="L:/Projects/Qt_test/" + cd.toString("dd.MM.yyyy") +".sql";
+    QStringList filters = {"*.sql"};
+    QDir dir;
+    std::vector<std::string> pathsFiles;
 
 };
 #endif // MAINWINDOW_H
